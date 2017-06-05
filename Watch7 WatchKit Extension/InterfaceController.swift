9210e5bfb43d21e633224abacc425f41fff9fb07
Michael Chapman <submariner100@gmail.com>
2017-06-05 20:44:28 +0100

    first commit

---
+++ Watch7 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Watch7 WatchKit Extension
+//
+//  Created by Macbook on 03/06/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
